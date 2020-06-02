f2c4bee0c9403820a5539c5a41d844ab90ea0d85
shyptr <shyptr14@gmail.com>
2020-06-02 22:15:33 +0800

    64

---
+++ 每日一题/面试题64. 求1+2+…+n/main.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+// 求 1+2+...+n ，要求不能使用乘除法、for、while、if、else、switch、case等关键字及条件判断语句（A?B:C）。
+//
+//
+//
+//示例 1：
+//
+//输入: n = 3
+//输出: 6
+//示例 2：
+//
+//输入: n = 9
+//输出: 45
+//
+//来源：力扣（LeetCode）
+//链接：https://leetcode-cn.com/problems/qiu-12n-lcof
+//著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+func sumNums(n int) int {
+	var ans int
+	var sum func(int) bool
+	sum = func(num int) bool {
+		ans += num
+		return num > 0 && sum(num-1)
+	}
+	sum(n)
+	return ans
+}
+
+func main() {
+	fmt.Println(sumNums(3))
+}
